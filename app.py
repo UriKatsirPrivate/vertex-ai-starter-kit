5fd2c5ff0cf75d54852a1e8f594fb64ac7755003
Uri Katsir <46541339+UriKatsirPrivate@users.noreply.github.com>
2024-02-13 21:49:07 +0200

    Initial

---
+++ app.py
@@ -0,0 +1,62 @@
+# https://huggingface.co/mistralai/Mistral-7B-v0.1
+
+import os
+# import requests
+from langchain.prompts import PromptTemplate
+import vertexai
+from langchain_google_vertexai import VertexAI
+from flask import request
+
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+def initialize_llm():
+    vertexai.init(project="landing-zone-demo-341118", location="us-central1")
+
+    return VertexAI(
+        model_name='gemini-pro',
+        max_output_tokens=8192,
+        temperature=0.3,
+        top_p=0.8,
+        top_k=23,
+        verbose=True,
+    )
+
+@app.route('/')
+def hello():
+    output = """
+    This is the default message. Use /query?question=" to pass your question to the LLM
+    """
+
+    return output
+
+
+# http://127.0.0.1:8080/query?question=list GCE instances
+@app.route('/query')
+def query_llm():
+    llm=initialize_llm()
+    template = """
+        System: You are a virtual assistant capable of generating the corresponding Google Cloud Platform (GCP) command-line interface (CLI) command based on the user's input.
+
+        Question: the user's input is: {user_input}.
+                  Please generate the corresponding GCP CLI command. Be as elaborate as possible and use as many flags as possible.
+                  For every flag you use, explain its purpose. also, make sure to provide a working sample command.
+
+        Answer:
+        """
+
+    prompt = PromptTemplate.from_template(template)
+
+    chain = prompt | llm
+
+    question = request.args.get('question')
+
+    output = chain.invoke({"user_input":question})
+
+    return output
+
+if __name__ == '__main__':
+    server_port = os.environ.get('PORT', '8080')
+    app.run(debug=False, port=server_port, host='0.0.0.0')
---
+++ deploy.sh
@@ -0,0 +1,32 @@
+# https://cloud.google.com/sdk/gcloud/reference/run/deploy
+export GOOGLE_CLOUD_PROJECT=landing-zone-demo-341118
+gcloud config set project $GOOGLE_CLOUD_PROJECT
+export SERVICE_NAME=vertex-ai-strater-kit
+export ARTIFACT_REGISTRY_NAME=cloud-run-source-deploy
+export REGION=me-west1
+export SERVICE_ACCOUNT_EMAIL=experts-hub-demo@landing-zone-demo-341118.iam.gserviceaccount.com
+
+# Enable APIs
+gcloud services enable cloudbuild.googleapis.com --project $GOOGLE_CLOUD_PROJECT
+gcloud services enable artifactregistry.googleapis.com --project $GOOGLE_CLOUD_PROJECT
+gcloud services enable run.googleapis.com --project $GOOGLE_CLOUD_PROJECT --async
+gcloud services enable aiplatform.googleapis.com --project $GOOGLE_CLOUD_PROJECT --async
+
+# Artifact Registry
+gcloud builds submit --tag $REGION-docker.pkg.dev/$GOOGLE_CLOUD_PROJECT/$ARTIFACT_REGISTRY_NAME/$SERVICE_NAME:latest
+
+gcloud run deploy $SERVICE_NAME \
+--image $REGION-docker.pkg.dev/$GOOGLE_CLOUD_PROJECT/$ARTIFACT_REGISTRY_NAME/$SERVICE_NAME:latest \
+--platform managed \
+--allow-unauthenticated \
+--region=$REGION \
+--ingress=all \
+--min-instances=0 \
+--concurrency=20 \
+--service-account=$SERVICE_ACCOUNT_EMAIL \
+--execution-environment=gen2    \
+--cpu-boost \
+--cpu=1 \
+--memory=1Gi \
+# --update-env-vars PROJECT_ID="landing-zone-demo-341118" \
+
